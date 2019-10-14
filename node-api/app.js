2290820eabb178df04e6705753c011990d579d95
Blake Spiers <blakespiers93@gmail.com>
2019-10-14 11:46:49 +1100

    L53 Showing all users

--- node-api/app.js
+++ node-api/app.js
@@ -19,6 +19,7 @@ mongoose.connection.on('err', err => {
 // bring in posts
 const postRoutes = require('./routes/post');
 const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 
 // middleware
 app.use(morgan("dev"));
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use(expressValidator());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
+app.use("/", userRoutes);
 app.use(function(err, req, res, next){
     if(err.name === 'UnauthorizedError'){
         res.status(401).json({error: "Unauthorized!"});
