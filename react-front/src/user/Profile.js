7450d18629ee9b84ca94af8877661d87c9c0fe44
Blake Spiers <blakespiers93@gmail.com>
2019-10-19 10:58:07 +1100

    L117 Code refactoring fetch user

--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {isAuthenticated} from '../auth'
 import {Redirect} from 'react-router-dom'
+import {read} from './apiUser'
 
 export default class Profile extends Component {
     constructor(){
@@ -11,26 +12,20 @@ export default class Profile extends Component {
         }
     }
 
-    componentDidMount() {
-        const userId = this.props.match.params.userId
-        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${isAuthenticated().token}`
-            }
-        })
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
+    init = (userId) => {
+        const token = isAuthenticated().token
+        read(userId, token).then(data => {
             if(data.error){
                 this.setState({redirectToSignin: true})
             } else {
                 this.setState({user: data})
             }
         })
+    }
+
+    componentDidMount() {
+        const userId = this.props.match.params.userId
+        this.init(userId)
     }
 
     render() {
