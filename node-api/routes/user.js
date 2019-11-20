f9979fbf3cc8bd00b5a6e1c037ea7956b36151b2
Blake Spiers <blakespiers93@gmail.com>
2019-11-20 18:42:04 +1100

    L159 Node API - Who to follow?
    Added back-end support for follow suggestions

--- node-api/routes/user.js
+++ node-api/routes/user.js
@@ -1,5 +1,17 @@
 const express = require('express');
-const {userById, allUsers, getUser, updateUser, deleteUser, userPhoto, addFollowing, addFollower, removeFollowing, removeFollower} = require("../controllers/user");
+const {
+    userById, 
+    allUsers, 
+    getUser, 
+    updateUser, 
+    deleteUser, 
+    userPhoto, 
+    addFollowing, 
+    addFollower, 
+    removeFollowing, 
+    removeFollower,
+    findPeople
+} = require("../controllers/user");
 const {requireSignin} = require('../controllers/auth');
 
 const router = express.Router();
@@ -13,6 +25,9 @@ router.put("/user/:userId", requireSignin, updateUser);
 router.delete("/user/:userId", requireSignin, deleteUser);
 //photo
 router.get("/user/photo/:userId", userPhoto)
+
+//who to follow
+router.get("/user/findpeople/:userId", requireSignin, findPeople)
 
 //any route containing :userID, our app will first execute userById()
 router.param("userId", userById);
