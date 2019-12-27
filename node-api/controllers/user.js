0bf6bf002016acc05a3ad3b0a5463a13e4eb2470
Blake Spiers <blakespiers93@gmail.com>
2019-12-27 16:28:29 +1100

    L214 Making a user admin using command line

--- node-api/controllers/user.js
+++ node-api/controllers/user.js
@@ -33,7 +33,7 @@ exports.allUsers = (req, res) => {
             });
         }
         res.json(users);
-    }).select("name email updated created");
+    }).select("name email updated created role");
 };
 
 exports.getUser = (req, res) => {
